5ba0a549711f0fe1460d0fd50637590f9cd81fad
JaewooByun <jaewoobyun1@gmail.com>
2020-01-21 15:04:43 +0900

    Initial Commit

---
+++ SafariTests/SafariTests.swift
@@ -0,0 +1,34 @@
+//
+//  SafariTests.swift
+//  SafariTests
+//
+//  Created by 변재우 on 20200121//.
+//  Copyright © 2020 변재우. All rights reserved.
+//
+
+import XCTest
+@testable import Safari
+
+class SafariTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
